8c32fbc683b706491c8a7b1b39968e2e290fc3ed
HopeSaurus <hopeXime@outlook.com>
2025-09-01 11:19:01 -0600

    Up to exercise 3.21

--- models/person.js
+++ models/person.js
@@ -15,8 +15,23 @@ mongoose.connect(url)
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+
+    number: {
+      type: String,
+      validate:{
+        validator: function(v){
+          return /\d{2,3}-\d+/.test(v)
+        },
+        message: props => `${props.value} has invalid format`
+      },
+      minLength: 8,
+      required: [true, 'User phone number required']
+    }
   })
 
   personSchema.set('toJSON', {
